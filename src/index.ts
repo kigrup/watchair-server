c77db59c2c658dbe6a38442fcf6408f5c0d03446
Raul <raul.plesa@gmail.com>
2022-08-02 18:55:40 +0200

    Demo tweak

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ app.use(express.json())
 
 const PORT = 3000
 
-app.get('/ping', (_req, res) => {
-  console.log('pinged')
-  res.send('ponged')
+app.get('/', (_req, res) => {
+  console.log('Incoming connection...')
+  res.send('WatChair')
 })
 
 app.listen(PORT, () => {
