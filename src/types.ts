2b70df7d3a3787ff4d43476375c6ba7e71d64c16
Raul <raul.plesa@gmail.com>
2022-08-05 18:18:16 +0200

    Added types

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,14 @@
-export type Weather = 'sunny' | 'rainy'
+export interface User {
+  username: string
+  hash: string
+  fullname: string
+}
+
+export interface Domain {
+  id: string
+}
+
+export interface Metric {
+  id: string
+  DomainId: string
+}
