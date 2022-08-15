e8a56b06d5bc4f8ff169c49494a5cd4f4df2f827
Raul Plesa <raul.plesa@gmail.com>
2022-08-15 12:14:59 +0200

    Added User type

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,68 @@
-export interface User {
-  username: string
-  hash: string
-  fullname: string
+import { Model, InferAttributes, InferCreationAttributes, ForeignKey, CreationOptional, DataTypes, Sequelize } from 'sequelize'
+
+export const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './database.sqlite'
+})
+
+// Class declarations
+
+export class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
+  declare username: string
+  declare hash: string
+  declare fullName: string
+
+  declare createdAt: CreationOptional<Date>
+  declare updatedAt: CreationOptional<Date>
 }
 
-export interface Domain {
-  id: string
+export class Domain extends Model<InferAttributes<Domain>, InferCreationAttributes<Domain>> {
+  declare id: string
+  declare ownerUsername: ForeignKey<User['username']>
+
+  declare createdAt: CreationOptional<Date>
+  declare updatedAt: CreationOptional<Date>
 }
 
-export interface Metric {
-  id: string
-  DomainId: string
+export class Metric extends Model<InferAttributes<Metric>, InferCreationAttributes<Metric>> {
+  declare id: string
+  declare domainId: ForeignKey<Domain['id']>
 }
+
+export class Dashboard extends Model<InferAttributes<Dashboard>, InferCreationAttributes<Dashboard>> {
+  declare id: string
+}
+
+// Classes initializations
+
+User.init(
+  {
+    username: {
+      type: DataTypes.STRING(128),
+      autoIncrement: false,
+      primaryKey: true
+    },
+    hash: {
+      type: new DataTypes.STRING(128),
+      allowNull: false
+    },
+    fullName: {
+      type: new DataTypes.STRING(256),
+      allowNull: false
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
+  },
+  {
+    tableName: 'users',
+    sequelize
+  }
+)
+
+// Associations
+
+User.hasMany(Domain, {
+  sourceKey: 'username',
+  foreignKey: 'ownerUsername',
+  as: 'domains'
+})
