e00a15161b3ab456bfc4a770d01a80234b707dd0
Raul <raul.plesa@gmail.com>
2022-08-17 10:56:09 +0200

    Added file upload

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import express from 'express'
+import formidable from 'formidable'
+import fs from 'fs'
+import crypto from 'crypto'
+
 const app = express()
 app.use(express.json())
 
@@ -6,7 +10,37 @@ const PORT = 3000
 
 app.get('/ping', (_req, res) => {
   console.log('Incoming connection...')
+  console.log(__dirname)
   res.send('WatChair')
+})
+
+app.get('/upload', (_req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.write('<form action="/api/fileupload" method="post" enctype="multipart/form-data">')
+  res.write('<input type="file" name="filetoupload"><br>')
+  res.write('<input type="submit">')
+  res.write('</form>')
+  return res.end()
+})
+
+app.post('/api/fileupload', (req, res) => {
+  const form = new formidable.IncomingForm()
+  form.parse(req, function (err, _fields, files) {
+    if (err != null) throw err
+    if (!Array.isArray(files.filetoupload)) {
+      const oldpath = files.filetoupload.filepath
+      const originalname = files.filetoupload.originalFilename != null ? files.filetoupload.originalFilename : 'uploadedfile'
+      const extension = originalname.substring(originalname.indexOf('.'))
+      const newpath = `${__dirname.replace('\\', '/')}/data/uploads/${crypto.randomUUID()}${extension}`
+      fs.rename(oldpath, newpath, function (err) {
+        if (err != null) throw err
+        res.write('File uploaded and moved!')
+        res.end()
+      })
+    }
+    res.write('File uploaded!')
+    res.end()
+  })
 })
 
 app.listen(PORT, () => {
