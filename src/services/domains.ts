aa00d01ddf812e83ea98f3d96e7b8356e1a2cdf9
Raul <raul.plesa@gmail.com>
2022-11-07 22:14:40 +0100

    Added domain getter

--- src/services/domains.ts
+++ src/services/domains.ts
@@ -1,6 +1,21 @@
 import { nanoid } from 'nanoid'
 import { inspect } from 'util'
+import { NotFoundError } from '../errors/not-found'
 import { Domain } from '../types'
+
+export const getDomain = async (domainId: string): Promise<Domain> => {
+  const domain = await Domain.findOne({
+    where: {
+      id: domainId
+    }
+  })
+
+  if (domain === null) {
+    throw new NotFoundError('Invalid Domain Id.')
+  }
+
+  return domain
+}
 
 export const createDomain = async (name: string): Promise<Domain> => {
   const newDomain: Domain = await Domain.create({
