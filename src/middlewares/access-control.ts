518e0727adb01e5f6d26e855950b5ff5580b1c99
Raul Plesa <raul.plesa@gmail.com>
2022-11-19 10:18:11 +0100

    Added DELETE to CORS

--- src/middlewares/access-control.ts
+++ src/middlewares/access-control.ts
@@ -3,6 +3,6 @@ import { RequestHandler } from 'express'
 export const corsAllow: RequestHandler = (_req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*') // TODO: update to match the domain you will make the requests from
   res.header('Access-Control-Allow-Headers', '*')
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST')
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
   next()
 }
