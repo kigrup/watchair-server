b5331afaf3461faa54b017b6cba83c97a3e40403
Raul <raul.plesa@gmail.com>
2022-11-22 22:52:26 +0100

    Added reviews route

--- src/routes/domains.ts
+++ src/routes/domains.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getDomainsHandler, createDomainHandler, createFileHandler, getDomainHandler, getPersonsHandler, deleteDomainHandler } from '../controllers/domains'
+import { getDomainsHandler, createDomainHandler, createFileHandler, getDomainHandler, getPersonsHandler, deleteDomainHandler, getReviewsHandler } from '../controllers/domains'
 import multer from 'multer'
 import { nanoid } from 'nanoid'
 import { inspect } from 'util'
@@ -48,5 +48,7 @@ router.route('/:domainId')
 router.route('/:domainId/files').post(upload.single('file'), createFileHandler)
 
 router.route('/:domainId/persons').get(getPersonsHandler)
+
+router.route('/:domainId/reviews').get(getReviewsHandler)
 
 export { router as domainsRouter }
